b572f4386e7048adf5d760df44f963388ea76430
szymon-drzewiecki <249478@student.pwr.edu.pl>
2021-03-26 16:28:22 +0100

    rozpoczęcie prac nad zadaniem 2.

---
+++ Zadanie2/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie2
+{
+    class Class1
+    {
+        public int timestamp { get; set; }
+        public float PLN { get; set; }
+    }
+}
--- Zadanie2/Program.cs
+++ Zadanie2/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http;
 
 namespace Zadanie2
 {
@@ -10,6 +12,19 @@ namespace Zadanie2
     {
         static void Main(string[] args)
         {
+            string call = "http://openexchangerates.org/api/historical/2021-03-25.json?app_id=23e326842d4044d1a971b4fb0359c5a3";
+            Task<string> Task = LoadJSON(call);
+            string json = Task.Result;
+            Class1 obiektKlasy = JsonConvert.DeserializeObject<Class1>(json);
+            Console.WriteLine(obiektKlasy.timestamp + " " + Convert.ToString(obiektKlasy.PLN));
+            Console.Read();
+        }
+
+        static async Task<string> LoadJSON(string call)
+        {
+            HttpClient httpClient = new HttpClient();
+            string json = await httpClient.GetStringAsync(call);
+            return json;
         }
     }
 }
