72e80be7794853e07e879599aeaca4142fdd9778
szymon-drzewiecki <249478@student.pwr.edu.pl>
2021-03-30 16:55:39 +0200

    obsługa db

--- Zadanie2/Class1.cs
+++ Zadanie2/Class1.cs
@@ -3,12 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace Zadanie2
 {
-    class Class1
+    public class Dane
     {
+        public int Id { get; set; }
         public int timestamp { get; set; }
+
+        public Przeliczniki rates { get; set; }
+
+    }
+
+    public class Przeliczniki
+    {
         public float PLN { get; set; }
+        public float EUR { get; set; }
+        public float BTC { get; set; }
+        public float COP { get; set; }
     }
 }
--- Zadanie2/Program.cs
+++ Zadanie2/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using System.Net.Http;
+using System.Data.Entity;
 
 namespace Zadanie2
 {
@@ -15,8 +16,21 @@ namespace Zadanie2
             string call = "http://openexchangerates.org/api/historical/2021-03-25.json?app_id=23e326842d4044d1a971b4fb0359c5a3";
             Task<string> Task = LoadJSON(call);
             string json = Task.Result;
-            Class1 obiektKlasy = JsonConvert.DeserializeObject<Class1>(json);
-            Console.WriteLine(obiektKlasy.timestamp + " " + Convert.ToString(obiektKlasy.PLN));
+            Dane obiektKlasy = JsonConvert.DeserializeObject<Dane>(json);
+           // Console.WriteLine(obiektKlasy.timestamp + " " + Convert.ToString(obiektKlasy.rates.PLN) + " " + Convert.ToString(obiektKlasy.rates.BTC));
+           // Console.Read();
+
+            var context = new KlasaGlowna();
+            context.ZestawyDanych.Add(obiektKlasy);
+            context.SaveChanges();
+
+            var dane = context.ZestawyDanych.Where(d => d.rates.PLN > 0).ToList<Dane>();
+
+            // var rates = context.ZestawyDanych.SqlQuery("select * from ZestawyDanych").ToList<Dane>();
+
+
+            foreach (var d in dane)
+                Console.WriteLine("PLN: {0}", d.rates.PLN);
             Console.Read();
         }
 
@@ -26,5 +40,14 @@ namespace Zadanie2
             string json = await httpClient.GetStringAsync(call);
             return json;
         }
+
+        public class KlasaGlowna: DbContext
+        {
+            public virtual DbSet<Dane> ZestawyDanych { get; set; }
+        }
+
+
+
+
     }
 }
