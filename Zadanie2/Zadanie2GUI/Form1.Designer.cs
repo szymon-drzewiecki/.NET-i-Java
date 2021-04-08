2c925be80218987faf1e04e5843c1df01b9919d0
szymon-drzewiecki <249478@student.pwr.edu.pl>
2021-04-08 23:12:28 +0200

    finito

---
+++ Zadanie2/Zadanie2GUI/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace Zadanie2GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(234, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(237, 400);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 383);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Check exchange rates";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "PLN",
+            "EUR",
+            "BTC",
+            "COP"});
+            this.listBox1.Location = new System.Drawing.Point(12, 120);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(200, 56);
+            this.listBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Choose currency";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 424);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Zadanie2/Zadanie2GUI/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Zadanie2;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Data.Entity;
+
+
+
+
+namespace Zadanie2GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var context = new KlasaGlowna();
+            string date_string = dateTimePicker1.Value.ToString("yyyy-MM-dd");
+            string currency = listBox1.GetItemText(listBox1.SelectedItem);
+
+
+            var ifExists = new List<Dane>();
+            ifExists = context.ZestawyDanych.Where(x => x.date == date_string).ToList<Dane>();
+            if (ifExists.Count != 0)
+            {
+                textBox1.Text = WriteByDay(context, date_string, currency);
+            }
+            else
+            {
+                textBox1.Text = "przed pobraniem";
+                string call = "http://openexchangerates.org/api/historical/" + date_string + ".json?app_id=23e326842d4044d1a971b4fb0359c5a3";
+                HttpClient httpClient = new HttpClient();
+                string json = await httpClient.GetStringAsync(call);
+                textBox1.Text = json;
+                Dane obiektKlasy = JsonConvert.DeserializeObject<Dane>(json);
+                obiektKlasy.date = date_string;
+                context.ZestawyDanych.Add(obiektKlasy);
+                context.SaveChanges();
+                textBox1.Text = WriteByDay(context, date_string, currency);
+
+
+
+            }
+        }
+
+
+
+
+        string WriteAll(KlasaGlowna context, string currency, string date_of_data)
+        {
+            string retString = "";
+            var allDownloaded = context.ZestawyDanych.Where(d => d.date == date_of_data).ToList<Dane>();
+            foreach (var d in allDownloaded)
+            {
+                /*
+                if (currency == "EUR")
+                retString += d.date + " " + "EUR:" + (d.rates.EUR).ToString();
+                else if (currency == "PLN")
+                retString += d.date + " " + "PLN:" + (d.rates.PLN).ToString();
+                */
+
+
+
+                retString = d.date + "       " + (d.rates.PLN).ToString() + Environment.NewLine;
+
+
+
+                /*
+                Console.WriteLine("Dane dla dnia: " + d.date);
+                Console.WriteLine("PLN: {0}", d.rates.PLN);
+                Console.WriteLine("EUR: {0}", d.rates.EUR);
+                Console.WriteLine("BTC: {0}", d.rates.BTC);
+                Console.WriteLine("COP: {0}", d.rates.COP);
+                */
+
+
+
+            }
+            return retString;
+        }
+
+
+        string WriteByDay(KlasaGlowna context, string date_of_data, string currency)
+        {
+            string retString = "";
+            var justDownloaded = context.ZestawyDanych.Where(d => d.date == date_of_data).ToList<Dane>();
+
+            if (currency == "PLN")
+                retString = justDownloaded[0].date + "   PLN: " + (justDownloaded[0].rates.PLN).ToString() + Environment.NewLine;
+            else if (currency == "EUR")
+                retString = justDownloaded[0].date + "   EUR: " + (justDownloaded[0].rates.EUR).ToString() + Environment.NewLine;
+            else if (currency == "BTC")
+                retString = justDownloaded[0].date + "   BTC: " + (justDownloaded[0].rates.BTC).ToString() + Environment.NewLine;
+            else if (currency == "COP")
+                retString = justDownloaded[0].date + "   COP: " + (justDownloaded[0].rates.COP).ToString() + Environment.NewLine;
+
+            return retString;
+        }
+
+        public class KlasaGlowna : DbContext
+        {
+            public virtual DbSet<Dane> ZestawyDanych { get; set; }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+
+}
